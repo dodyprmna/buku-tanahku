4a3dca269157786dcee58f1e6d8aa1875e2759f0
Dody Pramana <dodyprmna6@gmail.com>
2025-05-27 13:50:42 +0700

    kecamatan model

--- app/Models/Kecamatan.php
+++ app/Models/Kecamatan.php
@@ -7,6 +7,12 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Kecamatan extends Model
 {
+
+    protected $fillable = [
+        'id',
+        'nama_kecamatan'
+    ];
+
     function kelurahan() : hasMany {
         return $this->hasMany(Kelurahan::class);
     }
