5c4e16668194fb8c57ea0ad360fe8412ea590417
Dody Pramana <dodyprmna6@gmail.com>
2025-05-27 13:46:48 +0700

    model hari libur

---
+++ app/Models/HariLibur.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class HariLibur extends Model
+{
+    protected $fillable = [
+        'tanggal',
+        'keterangan'
+    ];
+}
